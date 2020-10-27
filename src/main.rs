de14e9b57f3b893cfdb262dfdd07e09c0e3990a1
Max Diamandopol <max.diamandopol@intenthq.com>
2020-10-27 15:21:40 +0000

    #2343 Refactor elastic search config

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use clap::{Arg, App};
 
-fn main() {
-    let app = App::new("writer").version("0.1").about("Bombards Elastic Search with batches of consolidated profiles or user IDs").author("Andrei Maximilian Diamandopol");
+struct ElasticSearchConfig {
+    host: String,
+    port: String
+}
 
-    let es_host_arg = Arg::with_name("elasticsearch-host").long("elasticsearch-host").help("Elasticsearch server host").takes_value(true).required(true);
-    let es_port_arg = Arg::with_name("elasticsearch-port").long("elasticsearch-port").help("Elasticsearch server port").takes_value(true).required(true);
+impl ElasticSearchConfig {
+    fn new() -> Self {
+        let app = App::new("writer").version("0.1").about("Bombards Elastic Search with batches of consolidated profiles or user IDs").author("Andrei Maximilian Diamandopol");
+
+        let es_host_arg = Arg::with_name("elasticsearch-host").long("elasticsearch-host").help("Elasticsearch server host").takes_value(true).required(true);
+        let es_port_arg = Arg::with_name("elasticsearch-port").long("elasticsearch-port").help("Elasticsearch server port").takes_value(true).required(true);
+        
+        let app = app.arg(es_host_arg);
+        let app = app.arg(es_port_arg);
     
-    let app = app.arg(es_host_arg);
-    let app = app.arg(es_port_arg);
+        let matches = app.get_matches();
 
-    let matches = app.get_matches();
+        let host = matches.value_of("elasticsearch-host").expect("This can't be None!");
+        let port = matches.value_of("elasticsearch-port").expect("This can't be None!");
 
-    let host = matches.value_of("elasticsearch-host").expect("This can't be None!");
-    let port = matches.value_of("elasticsearch-port").expect("This can't be None!");
-    println!("Host: {} Port: {}", host, port);
+        ElasticSearchConfig {host: host.to_string(), port: port.to_string()}
+    }
+}
+
+fn main() {
+    let elastic_search_config = ElasticSearchConfig::new();
+    
+    println!("Host: {} Port: {}", elastic_search_config.host, elastic_search_config.port); 
 }
